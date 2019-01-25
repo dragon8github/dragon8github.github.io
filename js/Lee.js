23c66579e88d0d38e3c6d2c7cf91ed6157f4f6be
lizhaohong <928532756@qq.com>
2019-01-25 17:44:12 +0800

    Site updated: 2019-01-25 17:44:10

--- js/Lee.js
+++ js/Lee.js
@@ -1,3 +1,4 @@
 $(function(){
-	$('article .article-inner').niceScroll({ cursorcolor: 'rgba(0,0,0,0.2)', autohidemode: true, horizrailenabled: false, emulatetouch: true })
+	$('article .article-inner').niceScroll({ cursorcolor: 'rgba(0,0,0,0.2)', autohidemode: true, horizrailenabled: false, emulatetouch: true, zindex: 199307100337 })
+	$('body').niceScroll({ cursorcolor: 'rgba(0,0,0,0.2)', autohidemode: false, horizrailenabled: false, emulatetouch: true, zindex: 199307100337 })
 })
